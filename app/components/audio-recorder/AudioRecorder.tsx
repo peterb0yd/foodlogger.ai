c30353e406609e634b100c05e1405716fcdd35be
PETER BOYD <peterboyd192@gmail.com>
2024-02-19 18:16:19 -0700

    update

--- app/components/audio-recorder/AudioRecorder.tsx
+++ app/components/audio-recorder/AudioRecorder.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useMemo } from "react";
 import { LinksFunction } from "@remix-run/node";
 import audioRecorderStyles from "./AudioRecorder.css";
 import { Button } from "../button/Button";
 import { Icons } from "~/enums/icons";
 import { useAudioRecorder } from "~/hooks/useAudioRecorder";
+import { useMobileDevice } from "~/hooks/useMobileDevice";
 
 const MIME_TYPE = "audio/webm";
 
@@ -18,14 +19,24 @@ interface AudioRecorderProps {
 
 export const AudioRecorder = ({ onStart, onStop }: AudioRecorderProps) => {
     const { startRecording, stopRecording, isRecording, audioURL } = useAudioRecorder();
+    const isMobile = useMobileDevice();
+    const clickProps = useMemo(() => {
+        if (isMobile) {
+            return {
+                onTouchStart: startRecording,
+                onTouchEnd: stopRecording,
+            };
+        }
+        return {
+            onMouseDown: startRecording,
+            onMouseUp: stopRecording,
+        };
+    }, [isMobile, startRecording, stopRecording]);
 
     return (
         <div className="AudioRecorder">
             <Button
-                // onTouchStart={startRecording}
-                onMouseDown={startRecording}
-                // onTouchEnd={stopRecording}
-                onMouseUp={stopRecording}
+                {...clickProps}
                 icon={Icons.RecordIcon}
                 iconColor={isRecording ? 'red' : 'gray'}
                 iconSize="xl"
---
+++ app/hooks/useMobileDevice.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useMobileDevice = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    const handleWindowSizeChange = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
+
+    return width <= 768;
+}
