89ca1a6c4dc74e65096909940e8528e6134ab313
PETER BOYD <peterboyd192@gmail.com>
2024-02-19 17:13:06 -0700

    test highlight

--- app/hooks/useAudioRecorder.tsx
+++ app/hooks/useAudioRecorder.tsx
@@ -5,6 +5,7 @@ const MIME_TYPE = "audio/webm";
 export const useAudioRecorder = (isRecording: boolean) => {
     const [audioURL, setAudioURL] = useState<string>("");
     const mediaRecorder = useRef<MediaRecorder | null>(null);
+    const chunks = useRef<Blob[]>([]);
 
     useEffect(() => {
         if (!mediaRecorder.current) return;
@@ -18,21 +19,20 @@ export const useAudioRecorder = (isRecording: boolean) => {
 
     const setupAudioRecorder = async () => {
         try {
-            let chunks: Blob[] = [];
             const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
             mediaRecorder.current = new MediaRecorder(stream as MediaStream);
             mediaRecorder.current.ondataavailable = (e) => {
                 if (typeof e.data === "undefined") return;
                 if (e.data.size === 0) return;
-                chunks.push(e.data);
+                chunks.current.push(e.data);
             }
             mediaRecorder.current.onstop = () => {
                 //creates a blob file from the audiochunks data
-                const audioBlob = new Blob(chunks, { type: mediaRecorder.current?.mimeType });
+                const audioBlob = new Blob(chunks.current, { type: mediaRecorder.current?.mimeType });
                 //creates a playable URL from the blob file.
                 const audioUrl = window.URL.createObjectURL(audioBlob);
                 setAudioURL(audioUrl);
-                chunks = [];
+                chunks.current = [];
             };
         } catch (err) {
             console.error(`The following getUserMedia error occurred: ${err}`);
