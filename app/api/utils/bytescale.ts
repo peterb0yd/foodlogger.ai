d8b334abb90aee6b1fdba34f6c84cd083a681a7c
PETER BOYD <peterboyd192@gmail.com>
2024-02-26 20:52:08 -0700

    test mobile

---
+++ app/api/utils/bytescale.ts
@@ -0,0 +1,9 @@
+import * as Bytescale from "@bytescale/sdk";
+
+export const bytescaleUploader = new Bytescale.UploadManager({
+    apiKey: process.env.BYTE_SCALE_API_KEY as string,
+})
+
+export const bytescaleProcesser = new Bytescale.FileApi({
+    apiKey: process.env.BYTE_SCALE_API_KEY as string,
+})
--- app/routes/api.food-logs.$id.food-item-logs.ts
+++ app/routes/api.food-logs.$id.food-item-logs.ts
@@ -1,7 +1,11 @@
 import { ActionFunction, LoaderFunction, json } from '@remix-run/node';
 import { FoodLogItemService } from '~/api/modules/food-log-item/food-log-item.service';
 import { convertAudioDataToReadStream } from '~/api/modules/food-log-item/food-log-item.utils.server';
+import { bytescaleUploader, bytescaleProcesser } from '~/api/utils/bytescale';
+import { AUDIO_FILE_EXT, MIME_TYPE } from '~/api/utils/constants';
 import { RequestMethods } from '~/enums/requests';
+import fs from 'fs';
+import { Readable } from 'stream';
 
 export const loader: LoaderFunction = async () => {
 	return new Response(null, {
@@ -17,11 +21,30 @@ export const action: ActionFunction = async (context) => {
 			try {
 				// Find out what the user said and create one or more food logs from it
 				// const formData = await parseMultipartFormData(request);
-                const formData = await request.formData();
+				const formData = await request.formData();
 				const audioString = formData.get('audio') as string;
-                const audioFile = await convertAudioDataToReadStream(audioString);
+
+				const audioReadStream = await convertAudioDataToReadStream(audioString);
+				const fileSize = fs.statSync(`/tmp/audio.${AUDIO_FILE_EXT}`).size;
+
+                const FILE_NAME = `/tmp/audio.${AUDIO_FILE_EXT}`;
+
+				const audioFile = await bytescaleUploader.upload({
+					data: audioReadStream,
+					size: fileSize,
+					originalFileName: FILE_NAME, // 'audio.wav
+					mime: MIME_TYPE,
+				});
+
+                // Uint8Array
+                const audioWriteStream = fs.createWriteStream(FILE_NAME);
+                const audioUrl = audioFile.fileUrl;
+                const response = await fetch(`${audioUrl}?f=mp3`);
+                await responseToReadable(response)?.pipe(audioWriteStream);
+                const readStream = await fs.createReadStream(FILE_NAME);
+
 				const foodLogId = params.id as string;
-				const foodItemLog = await FoodLogItemService.create(audioFile, foodLogId);
+				const foodItemLog = await FoodLogItemService.create(readStream, foodLogId);
 				return json(foodItemLog);
 			} catch (error) {
 				console.error(error);
@@ -35,4 +58,23 @@ export const action: ActionFunction = async (context) => {
 			return new Response('Method not allowed', { status: 405 });
 		}
 	}
+};
+
+// TODO: move to util
+const responseToReadable = (response: Response) => {
+    if (!response.body) {
+        return null;
+    }
+    const reader = response.body.getReader();
+    const rs = new Readable();
+    rs._read = async () => {
+        const result = await reader.read();
+        if(!result.done){
+            rs.push(Buffer.from(result.value));
+        }else{
+            rs.push(null);
+            return;
+        }
+    };
+    return rs;
 };
