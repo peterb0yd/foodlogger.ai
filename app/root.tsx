3f6ce4ecd9cedead748f7b56fbe889442574c116
PETER BOYD <peterboyd192@gmail.com>
2024-05-05 17:46:31 -0600

    meta img

--- app/root.tsx
+++ app/root.tsx
@@ -38,8 +38,9 @@ export const meta: MetaFunction = () => {
         { name: "twitter:description", content: "Use FoodLogger.ai to quickly log your meals with voice recognition. The simplest way to track your diet!" },
         { name: "twitter:image", content: "https://www.foodlogger.ai/imgs/foodlogger-background.png" },
         // { name: "twitter:creator", content: "@yourtwitterhandle" },
-        { rel: "icon", href: "/imgs/foodlogger-fav-64.png", sizes: "64x64" },
-        { rel: "icon", href: "/imgs/foodlogger-fav-128.png", sizes: "128x128" },
+        { rel: "icon", href: "/imgs/foodlogger-fav-64.png", type:"image/png", sizes: "64x64" },
+        { rel: "icon", href: "/imgs/foodlogger-fav-128.png", type:"image/png", sizes: "128x128" },
+        { rel: "icon", href: "/imgs/foodlogger-fav-64.png", type:"image/png" },
         { rel: "apple-touch-icon", href: "/imgs/foodlogger-fav-128.png" },
         { rel: "manifest", href: "/manifest.webmanifest" }
     ];
