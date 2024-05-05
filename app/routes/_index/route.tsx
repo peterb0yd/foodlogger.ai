4f5002d5bea36f953675d7e61bf34828b76c3e61
PETER BOYD <peterboyd192@gmail.com>
2024-05-05 17:46:29 -0600

    meta img

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -35,26 +35,26 @@ export default function Home() {
                     icon={IconNames.ChevronCircle}
                     iconSide="right"
                 >
-                    Go to Logs
+                    Start tracking
                 </Button>
 
                 <List variant="gap-loose" bg="none">
                     <List.Item col align="left" padX="md">
-                        <Text size="lg" color="primary" weight="black">1. Voice</Text>
+                        <Text size="lg" color="primary" weight="black">1. Faster logging using voice</Text>
                         <Text size="md" color="highlight">
                             {`Voice recognition is easier than navigating a bunch of dropdowns and inputs. Say what you ate and let AI figure out the details.`}
                         </Text>
                     </List.Item>
                     <List.Item col align="left" padX="md">
-                        <Text size="lg" color="primary" weight="black">2. Privacy</Text>
+                        <Text size="lg" color="primary" weight="black">2. Easy sign up & log in</Text>
                         <Text size="md" color="highlight">
                             {`Tell us your name and email... or don't. We don't care. Log in and sign up with only a phone number. Password and personal info is not required.`}
                         </Text>
                     </List.Item>
                     <List.Item col align="left" padX="md">
-                        <Text size="lg" color="primary" weight="black">3. Template</Text>
+                        <Text size="lg" color="primary" weight="black">3. Convenience saves you time</Text>
                         <Text size="md" color="highlight">
-                            {`Logging can be annoying. Use templates to speed it up. Save your favorite snacks, drinks and meals and log them with a single button. Modify your log any way you want.`}
+                            {`Logging everything you eat is annoying. Make templates to speed it up. Save your favorite snacks, drinks and meals and log them with a single button.`}
                         </Text> 
                     </List.Item>
                 </List>
