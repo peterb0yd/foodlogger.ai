68b9a623e980ebf5698848500ec09112ea110e5f
Peter Boyd <peterboyd192@gmail.com>
2024-05-11 08:18:13 -0600

    homepage copy

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -23,9 +23,11 @@ export default function Home() {
     return (
         <Main name="Home">
             <FlexBox col gap="2xl" center padBottom="1/3">
-                <FlexBox col gap="md" align="center" width="full">
-                    <Text size="xl" align="center" weight="black" lineHeight="tight">{`Meal tracking made simple.`}</Text>
-                    <Text size="md" align="center" weight="bold" color="muted" lineHeight="tight">{`Tell AI what you ate and let it do your tracking for you.`}</Text>
+                <FlexBox col gap="lg" align="center" width="full">
+                    <Text size="2xl" align="center" weight="black" lineHeight="tight">{`Enhance your well-being.`}</Text>
+                    <Text size="md" align="center" weight="bold" color="muted" lineHeight="tight">
+                        {`Discover the foods that boost your Mood and Energy with the help of AI.`}
+                    </Text>
                 </FlexBox>
                 <Button
                     to={PageRoutes.LOGS}
@@ -40,34 +42,46 @@ export default function Home() {
 
                 <List variant="gap-loose" bg="none">
                     <List.Item col align="left" padX="md">
-                        <Text size="xl" color="primary" weight="bold">{`1. Fast logging using voice`}</Text>
+                        <Text size="xl" color="primary" weight="bold">
+                            {`1. Easy logging using voice`}
+                        </Text>
                         <Text size="md" color="highlight">
-                            {`Voice recognition is easier than navigating a bunch of dropdowns and inputs. Say what you ate and let AI figure out the details.`}
+                            {`Log your snacks, drinks and meals faster than ever with the help of Voice Recognition. Why waste your time typing food names and selecting quantities from dropdowns if there is an easier way?`}
                         </Text>
                     </List.Item>
                     <List.Item col align="left" padX="md">
-                        <Text size="xl" color="primary" weight="bold">{`2. Save time with templates`}</Text>
+                        <Text size="xl" color="primary" weight="bold">
+                            {`2. Save time with templates`}
+                        </Text>
                         <Text size="md" color="highlight">
                             {`It's true, logging everything you eat is time-consuming. Use templates to speed it up. Save your favorite snacks, drinks and meals and log them with a single button.`}
-                        </Text> 
+                        </Text>
                     </List.Item>
                     <List.Item col align="left" padX="md">
-                        <Text size="xl" color="primary" weight="bold">{`3. Track mood, energy, bowels and more`}</Text>
+                        <Text size="xl" color="primary" weight="bold">
+                            {`3. Track mood, energy, bowels and more`}
+                        </Text>
                         <Text size="md" color="highlight">
-                            {`It turns out what we eat impacts more than just our weight. Track your mood, bowel movements, energy levels, sleep and more to see how your diet affects your overall mood and health.`}
-                        </Text> 
+                            {`It turns out what we eat impacts more than just our weight. Track your mood, energy levels, bowel movements, sleep and more to see how your diet affects your overall mood and health.`}
+                        </Text>
                     </List.Item>
                     <List.Item col align="left" padX="md">
-                        <Text size="xl" color="primary" weight="bold">{`4. Easy sign up & log in`}</Text>
+                        <Text size="xl" color="primary" weight="bold">
+                            {`4. Easy sign up & log in`}
+                        </Text>
                         <Text size="md" color="highlight">
                             {`Input your name and email... or not. We don't care. Log in and sign up with only a phone number. Password and personal info is not required.`}
                         </Text>
                     </List.Item>
                     <List.Item col align="left" padX="md">
-                        <Text size="xl" color="primary" weight="bold">{`5. Image recognition (coming soon)`}</Text>
+                        <Text size="xl" color="primary" weight="bold">
+                            {`5. Calorie & macronutrient breakdown (coming soon)`}
+                        </Text>
                     </List.Item>
                     <List.Item col align="left" padX="md">
-                        <Text size="xl" color="primary" weight="bold">{`6. Calorie & macronutrient breakdown (coming soon)`}</Text>
+                        <Text size="xl" color="primary" weight="bold">
+                            {`6. Image recognition (coming soon)`}
+                        </Text>
                     </List.Item>
                 </List>
             </FlexBox>
